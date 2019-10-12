867e11533c27afbd22408e0f6fde1d3c41ac8697
BuildTools <psm7177@naver.com>
2019-10-12 20:49:49 +0900

    android init

---
+++ app/src/main/java/psm7177/tmsassistant/MainActivity.java
@@ -0,0 +1,14 @@
+package psm7177.tmsassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
